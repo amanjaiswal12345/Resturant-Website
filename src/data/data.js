4d72ce1e4675afc3a6655be29d262df93c0fc228
amanjaiswal12345 <aj269035@gmail.com>
2023-11-03 20:31:38 +0530

    first commit

---
+++ src/data/data.js
@@ -0,0 +1,51 @@
+import Dosa from "../images/dosa.jpg";
+import Chola from "../images/chhola.jpg";
+import Idli from "../images/idli.jpg";
+import MasalaDosa from "../images/masala.jpg";
+import Paneer from "../images/paneer.jpg";
+import Gujrati from "../images/gujrati.jpeg";
+
+export const MenuList = [
+  {
+    name: "Dosa",
+    description:
+      "Dosa: South Indian crepe made from rice and lentil batter, a popular and versatile dish.",
+    image: Dosa,
+    price: 200,
+  },
+  {
+    name: "Chola",
+    description:
+      "Chola food: Spicy, flavorful, vegetarian Indian cuisine with legumes and spices.",
+    image: Chola,
+    price: 250,
+  },
+  {
+    name: "Idli Sambhar",
+    description:
+      "Idli Sambhar: South Indian dish featuring steamed rice cakes with spicy lentil soup. Delicious breakfast!",
+    image: Idli,
+    price: 300,
+  },
+  {
+    name: "Masala Dosa",
+    description:
+      "Crispy, thin rice crepe filled with spiced potato, a South Indian favorite for breakfast or snack.",
+    image: MasalaDosa,
+    price: 100,
+  },
+  {
+    name: "Paneer",
+    description:
+      "Paneer: Indian cottage cheese, versatile, used in curries, snacks, and desserts. Rich source of protein.",
+    image: Paneer,
+    price: 400,
+  },
+  {
+    name: "Gujrati",
+    description:
+      "Gujarati food: Vibrant, vegetarian cuisine with sweet, spicy, and savory flavors, featuring dhokla, thepla..",
+    image: Gujrati,
+    price: 500,
+  },
+];
